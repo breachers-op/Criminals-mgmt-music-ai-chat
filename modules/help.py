9caeadb84f557344b86e2bf5f73b274e06ecf159
I am breachers <89657427+breachers-op@users.noreply.github.com>
2026-05-27 17:27:15 +0530

    Delete modules directory

--- modules/help.py
+++
@@ -1,6 +0,0 @@
-from hydrogram import filters
-from Criminals import app
-
-@app.on_message(filters.command("help"))
-async def help_cmd(client, message):
-    await message.reply("**Criminals Bot**\n/ai - Chat\n/mute /ban - Admin\n/setflood - Security\n/purge - Delete\nInline: @botname text")
