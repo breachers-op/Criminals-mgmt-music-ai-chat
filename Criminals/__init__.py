35d4acf6fa74bfcb506d646f924d80828fcf59a3
I am breachers <89657427+breachers-op@users.noreply.github.com>
2026-05-27 17:14:26 +0530

    Create __init__.py

---
+++ Criminals/__init__.py
@@ -0,0 +1,17 @@
+from hydrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN, SESSION_STRING
+
+app = Client(
+    "CriminalsBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="Criminals/modules")
+)
+
+assistant = Client(
+    "CriminalsAssistant",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=SESSION_STRING
+) if SESSION_STRING else None
