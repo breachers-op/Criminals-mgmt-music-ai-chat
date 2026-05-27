78bcfd1813fc62d1e337c6f77eccdda443629d62
I am breachers <89657427+breachers-op@users.noreply.github.com>
2026-05-27 17:13:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID", "12345"))
+API_ID = int(os.getenv("API_ID", "0"))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-SESSION_STRING = os.getenv("SESSION_STRING", "") # For Music Assistant
+SESSION_STRING = os.getenv("SESSION_STRING", "")
 MONGO_URL = os.getenv("MONGO_URL", "")
 OWNER_ID = int(os.getenv("OWNER_ID", "0"))
 SUDO_USERS = [int(x) for x in os.getenv("SUDO_USERS", "").split()] if os.getenv("SUDO_USERS") else []
