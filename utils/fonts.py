9615cbfa1ce492aced360c91b449f2736a5b9504
I am breachers <89657427+breachers-op@users.noreply.github.com>
2026-05-27 17:11:14 +0530

    Delete utils directory

--- utils/fonts.py
+++
@@ -1,7 +0,0 @@
-def stylish(text):
-    """Converts text to Small Caps font"""
-    mapping = str.maketrans(
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-        "ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ"
-    )
-    return text.translate(mapping)
