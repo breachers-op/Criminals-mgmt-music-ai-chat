5dde4b1b6a1adb3917020e032351bf4a59b1ba41
I am breachers <89657427+breachers-op@users.noreply.github.com>
2026-05-27 16:53:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,15 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID", "0"))
+API_ID = int(os.getenv("API_ID", "12345"))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-SESSION_STRING = os.getenv("SESSION_STRING", "")
+SESSION_STRING = os.getenv("SESSION_STRING", "") # For Music Assistant
 MONGO_URL = os.getenv("MONGO_URL", "")
-AI_API_KEY = os.getenv("AI_API_KEY", "")
 OWNER_ID = int(os.getenv("OWNER_ID", "0"))
-SUDO_USERS = [int(x) for x in os.getenv("SUDO_USERS", "").split()]
-SUDO_USERS.append(OWNER_ID)
-
-# UI Settings
-IMG_URL = "https://telegra.ph/file/0c32f8319688001d93198.jpg" # Replace with your banner
+SUDO_USERS = [int(x) for x in os.getenv("SUDO_USERS", "").split()] if os.getenv("SUDO_USERS") else []
+AI_API_KEY = os.getenv("AI_API_KEY", "")
