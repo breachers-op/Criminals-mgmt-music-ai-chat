8036c01bb14ff65f741e1f0df3ace2341ded422e
I am breachers <89657427+breachers-op@users.noreply.github.com>
2026-05-27 12:17:22 +0530

    Create fonts.py

---
+++ utils/fonts.py
@@ -0,0 +1,7 @@
+def stylish(text):
+    """Converts text to Small Caps font"""
+    mapping = str.maketrans(
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        "ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ"
+    )
+    return text.translate(mapping)
