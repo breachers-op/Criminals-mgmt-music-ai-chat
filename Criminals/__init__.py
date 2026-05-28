fdbae13e14fb6efde4fe63ec5657a639e9c10c70
I am breachers <89657427+breachers-op@users.noreply.github.com>
2026-05-28 15:37:45 +0530

    Update __init__.py

--- Criminals/__init__.py
+++ Criminals/__init__.py
@@ -1,12 +1,13 @@
 from hydrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN, SESSION_STRING
 
+# We use "Criminals.modules" as a python package path
 app = Client(
     "CriminalsBot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="Criminals/modules")
+    plugins=dict(root="Criminals.modules") 
 )
 
 assistant = Client(
